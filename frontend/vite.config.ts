9211f616c2fa1ede91cbd800c223efeeede842cd
bpognon <bastien.pognon@smsmode.com>
2026-03-30 14:12:03 +0200

    chore : change folders name

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,18 +1,14 @@
 import tailwindcss from "@tailwindcss/vite";
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { defineConfig } from "vitest/config";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
   server: {
-    proxy: {
-      "/api": {
-        target: "https://localhost:8000",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
+    host: "0.0.0.0", // ← this line is required for Docker
+    port: 5173,
+    strictPort: true,
   },
   test: {
     environment: "jsdom",
